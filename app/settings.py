c6e67f6dff4572882236e1c04ed102c44cbdd762
Gabriel Bonaretti <gabriel_bonaretti@hotmail.com>
2023-11-21 11:22:13 -0300

    coloquei a verificação de tentar entrar 3 vezes

--- app/settings.py
+++ app/settings.py
@@ -151,7 +151,13 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '3/minute',
+    },
 }
 
 SIMPLE_JWT = {
